2812f422be732bebf5a4d05edb081b496ea8d470
Krishna Avtar <krishnaavtar955@gmail.com>
2025-12-02 18:00:47 +0530

    change port

--- server/index.js
+++ server/index.js
@@ -25,5 +25,5 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
